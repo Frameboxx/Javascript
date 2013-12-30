5055377aa0e45e279f4dde47ad7118ebafe2f055
Sachin Bhatnagar <mailme@sachinbhatnagar.com>
2013-12-30 15:50:25 +0530

    Lecture 3 Uploaded

---
+++ Lecture3/scripts/loops.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+
+	var fruits = ["Apples","Oranges","Strawberries","Mango","Kiwi"];
+	
+	for(var i=0;i<fruits.length;i++){
+		console.log(fruits[i]);
+	}
+
+	for(var i=(fruits.length-1);i>=0;i--){
+		console.log(fruits[i]);
+	}
+
+}
